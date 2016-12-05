0f21ee2298a764d48ef89e4167a76cf147d75892
Ali Khalili <ali.khll.1994@gmail.com>
2016-12-05 15:17:27 +0330

    cluster event on exit added

--- app.ts
+++ app.ts
@@ -5,22 +5,25 @@ import * as os from "os";
 var cluster = require('cluster');
 
 if (cluster.isMaster) {
-
     var cpuCount = require('os').cpus().length;
     console.log(cpuCount);
 
     for (var i = 0; i < cpuCount; i++) {
         cluster.fork();
     }
-    
+
+    cluster.on('exit', (worker: any, code: any, signal: any) => {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
 }
 else {
-
     var app = express();
     var datas: { datas: Array<{ name: string }> } = require("./app/data/data.json");
 
     app.set("datas", datas);
-    
+
     app.use(express.static('public'));
     app.use(cookieParser());
     app.use(bodyParser.json());
@@ -28,13 +31,13 @@ else {
 
     app.use(require('./app/routes/crud'));
     app.get('/test1', (req, res) => {
-        for (let i = 0; i < 1000000000000000000; i++)
+        console.log("t1 : " + cluster.worker.id);
+        for (let i = 0; i < 100000000000000; i++)
         { }
-        res.end(`test`);
+        res.end(`test - ` + cluster.worker.id);
     });
     app.get('/test2', (req, res) => {
-
-        res.end(`test2`);
+        res.end(`test2 - ` + cluster.worker.id);
     });
 
     var server = app.listen(8081, () => {
