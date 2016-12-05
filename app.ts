ca68001ed0061d9316fea8332cf987d826cba58e
Ali Khalili <ali.khll.1994@gmail.com>
2016-12-05 11:31:15 +0330

    chandle clustering in initial app

--- app.ts
+++ app.ts
@@ -2,24 +2,45 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cookieParser from "cookie-parser";
 import * as os from "os";
-var app = express();
-var datas : {datas: Array<{name: string}>} = require("./app/data/data.json");
+var cluster = require('cluster');
 
-app.set("datas", datas);
+if (cluster.isMaster) {
 
-app.use(express.static('public'));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false })); //for post request
+    var cpuCount = require('os').cpus().length;
+    console.log(cpuCount);
 
-app.use(require('./app/routes/crud'));
-app.get('/test', (req, res) => {
-    let cpuCount = os.cpus().length;
-    res.end(`cpu count ${cpuCount}`);
-});
+    for (var i = 0; i < cpuCount; i++) {
+        cluster.fork();
+    }
+    
+}
+else {
 
-var server = app.listen(8081, () => {
-    var host = server.address().address
-    var port = server.address().port
-    console.log(`runs in port ${port}`);
-})
+    var app = express();
+    var datas: { datas: Array<{ name: string }> } = require("./app/data/data.json");
+
+    app.set("datas", datas);
+    
+    app.use(express.static('public'));
+    app.use(cookieParser());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false })); //for post request
+
+    app.use(require('./app/routes/crud'));
+    app.get('/test1', (req, res) => {
+        for (let i = 0; i < 1000000000000000000; i++)
+        { }
+        res.end(`test`);
+    });
+    app.get('/test2', (req, res) => {
+
+        res.end(`test2`);
+    });
+
+    var server = app.listen(8081, () => {
+        var host = server.address().address
+        var port = server.address().port
+        console.log(`runs in port ${port}`);
+    })
+
+}
